a7525e69ccdbbe86077b09fb5290e425a09e593f
Marco Endres <marco.endres@clavisit.com>
2020-07-03 14:24:10 +0200

    added eslint eslint-loader

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,12 @@ module.exports = {
   module: {
     rules: [
       {
+        enforce: 'pre',
+        test: /\.(js|vue)$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/
+      },
+      {
         test: /\.js?$/,
         exclude: /node_modules/,
         use: 'babel-loader'
